3dccd1382b9f8026a0771ac489b47d59e3853c3d
Dhany Dwi Putra <dhany.dwi@nabatisnack.co.id>
2025-03-17 16:28:24 +0700

    Adding prometheus http

--- main.go
+++ main.go
@@ -4,18 +4,62 @@ import (
 	"shollu/config"
 	"shollu/database"
 	"shollu/routes"
+	"strconv"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/adaptor"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
+
+// Buat metrik Prometheus
+var (
+	httpRequestsTotal = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "http_requests_total",
+			Help: "Total jumlah HTTP request",
+		},
+		[]string{"method", "path", "status"},
+	)
+
+	httpRequestDuration = prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Name:    "http_request_duration_seconds",
+			Help:    "Durasi HTTP request dalam detik",
+			Buckets: prometheus.DefBuckets,
+		},
+		[]string{"method", "path"},
+	)
+)
+
+func init() {
+	// Register metrik ke Prometheus
+	prometheus.MustRegister(httpRequestsTotal)
+	prometheus.MustRegister(httpRequestDuration)
+}
 
 func main() {
 	config.LoadConfig()
 	database.Connect()
 
 	app := fiber.New()
+
+	// Middleware untuk mengukur request dan latensi
+	app.Use(func(c *fiber.Ctx) error {
+		method := c.Method()
+		path := c.Path()
+
+		timer := prometheus.NewTimer(httpRequestDuration.WithLabelValues(method, path))
+		defer timer.ObserveDuration()
+
+		err := c.Next()
+
+		status := c.Response().StatusCode()
+		httpRequestsTotal.WithLabelValues(method, path, strconv.Itoa(status)).Inc()
+
+		return err
+	})
 
 	app.Use(cors.New(cors.Config{
 		AllowOrigins: "*", // Izinkan semua domain (ganti dengan domain frontend jika perlu)
