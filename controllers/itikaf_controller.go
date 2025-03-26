fcc3592a62137492e9e710d5acbc3b4e3263b1c1
Dhany Dwi Putra <dhany.dwi@nabatisnack.co.id>
2025-03-26 08:15:17 +0700

    Change timeUTC sholat champions

--- controllers/itikaf_controller.go
+++ controllers/itikaf_controller.go
@@ -107,8 +107,8 @@ func GetRekapAbsen(c *fiber.Ctx) error {
 			LEFT JOIN petugas ON absensi.mesin_id = petugas.id_user
 			LEFT JOIN peserta ON absensi.user_id = peserta.id
 			WHERE absensi.event_id = ? AND petugas.id_masjid = ?
-			AND DATE(absensi.created_at) = DATE(?)
-			AND TIME(absensi.created_at) BETWEEN ? AND ? GROUP BY absensi.user_id, peserta.fullname`
+			AND DATE(CONVERT_TZ(absensi.created_at, '+00:00', '+07:00')) = DATE(?)
+			AND TIME(CONVERT_TZ(absensi.created_at, '+00:00', '+07:00')) BETWEEN ? AND ? GROUP BY absensi.user_id, peserta.fullname`
 		args = append(args, idEvent, idMasjid, tanggal, jamMin, jamMax)
 	default:
 		return c.Status(http.StatusBadRequest).JSON(fiber.Map{"error": "Invalid event_id"})
